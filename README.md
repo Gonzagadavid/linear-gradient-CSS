8d5dd906a719038a771e1a0d55ac351bb1169b8c
Gonzagadavid <david.blesses@hotmail.com>
2021-04-15 12:32:56 -0300

    adicionado comentarios no codigo js e imagens e instruções no README

--- README.md
+++ README.md
@@ -10,3 +10,15 @@
 - Opção de escolher o anglo;
 - Disponibiliza o codigo linear-gradient em RGB pronto para colocar em seu projeto;
 - Botão para copiar direto para área de transferência.
+
+
+
+## Intruções de uso:
+
+1. Clone o repositorio em sua máquina :octocat: :octocat:
+2. A abra o arquivo index.html no browser
+3. escolha a primeira cor
+4. escolha a segunda cor
+5. escolha o anglo, podendo usar os botões de 15 em 15 ou alterar diretamente no display;
+6. clique no botão 'copy'
+7. agora é só colar em seu CSS
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// constantes de elementos do DOM e variaveis usada para eventos
 const color1 = document.getElementById('color1')
 const color2 = document.getElementById('color2')
 const background = document.getElementById('background')
@@ -11,11 +12,15 @@ let cor1 = 'rgb(255, 255, 255)'
 let cor2 = 'rgb(0, 0, 0)'
 let angle = 0
 
+// eventos de elementos do DOM
 color1.addEventListener('input', e => displayColor(e.target.value, color1, '1'))
 color2.addEventListener('input', e => displayColor(e.target.value, color2, '2'))
 deg.addEventListener('input', e => updateAngle(e.target.value))
 copy.addEventListener('click', e => copyCode())
+add.addEventListener('click', e => updateDeg(add, '+'))
+less.addEventListener('click', e => updateDeg(less, '-'))
 
+// copia o codigo disponivel para a área de transferencia
 function copyCode () {
   code.select()
   document.execCommand('copy')
@@ -25,21 +30,21 @@ function copyCode () {
   copy.removeEventListener('click', e => copyCode())
 }
 
-add.addEventListener('click', e => updateDeg(add, '+'))
-less.addEventListener('click', e => updateDeg(less, '-'))
-
+// aumenta ou diminui 15 graus no anglo de acordo com o botão
 function updateDeg (btn, str) {
   angle = str === '+' ? angle + 15 : angle - 15
   updateBG()
   btn.removeEventListener('click', e => updateDeg(less, str))
 }
 
+// atualiza o valor do anglo quando alterado diretamente
 function updateAngle (value) {
   angle = value
   updateBG()
   deg.removeEventListener('input', e => updateAngle(e.target.value))
 }
 
+// atualiza a cor de acordo com a cor escolhida
 function displayColor (hex, cor, n) {
   const rgb = toRGB(hex)
   n === '1' ? cor1 = rgb : cor2 = rgb
@@ -47,6 +52,7 @@ function displayColor (hex, cor, n) {
   cor.removeEventListener('input', e => displayColor(e.target.value, cor))
 }
 
+// transforma o codigo da cor de hexadecimal para rgb 
 function toRGB (hex) {
   const R = parseInt(hex[1] + hex[2], 16)
   const G = parseInt(hex[3] + hex[4], 16)
@@ -54,12 +60,14 @@ function toRGB (hex) {
   return `rgb(${R}, ${G}, ${B})`
 }
 
+// atualiza a cor e o anglo de fundo conforme a escolha do usuario
 function updateBG () {
   deg.value = angle
   code.value = `linear-gradient(${angle}deg, ${cor1}, ${cor2})`
   background.style.background = `linear-gradient(${angle}deg, ${cor1}, ${cor2})`
 }
 
+// retira a mensagem de codigo copiado após 1 segundo
 function messageNone () {
   setTimeout(_ => {
     box.style.display = 'none'
